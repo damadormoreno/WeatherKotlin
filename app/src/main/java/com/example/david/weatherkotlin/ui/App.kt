927938bd41e6741d4410fad16309325b99302017
David Amador Moreno <amador.moreno.david@gmail.com>
2017-08-27 15:25:17 +0200

    cambio en app

--- app/src/main/java/com/example/david/weatherkotlin/ui/App.kt
+++ app/src/main/java/com/example/david/weatherkotlin/ui/App.kt
@@ -6,6 +6,7 @@ import com.example.david.weatherkotlin.ui.utils.DelegatesExt
 /**
  * Created by david on 27/08/17.
  */
+
 class App: Application(){
     companion object {
         var instance : App by DelegatesExt.notNullSingleValue()
